84613c74d01706b63518943382a81dcb51924583
sammisam8888 <samuelpriyatam@gmail.com>
2025-02-26 00:51:56 +0530

    Add QR code scanner and face scanner plugins; remove fingerprint authentication components

--- frontend/attendance_app/lib/main.dart
+++ frontend/attendance_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,33 +12,63 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Fingerprint Authentication',
+      title: 'QR Code Scanner',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(), // Start with the login screen
+      home: QRScannerScreen(),
     );
   }
 }
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class QRScannerScreen extends StatefulWidget {
+  const QRScannerScreen({super.key});
+
+  @override
+  _QRScannerScreenState createState() => _QRScannerScreenState();
+}
+
+class _QRScannerScreenState extends State<QRScannerScreen> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+  String qrText = '';
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Center(
-        child: IconButton(
-          key: Key('fingerprintButton'), // Add this key
-          icon: Icon(Icons.fingerprint),
-          onPressed: () {
-            // Trigger fingerprint authentication
-          },
-        ),
+      appBar: AppBar(title: Text('QR Code Scanner')),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Text('Scan result: $qrText'),
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        qrText = scanData.code!;
+      });
+    });
   }
 }
