0952c888be403a4c90aa53f28486ec9845381c6c
sammisam8888 <samuelpriyatam@gmail.com>
2025-03-01 14:04:18 +0530

    rectified-all-flutter-android errors

--- frontend/attendance_app/android/app/build.gradle.kts
+++ frontend/attendance_app/android/app/build.gradle.kts
@@ -1,26 +1,46 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    compileSdk = 34
+    namespace = "com.example.attendance_app"
+    compileSdk = 35
 
     defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.attendance_app"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+}
+
+flutter {
+    source = "../.."
 }
 
 dependencies {
